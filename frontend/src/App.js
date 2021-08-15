49e5dc4c9523e436828dfed640a6dc0684c7c2ab
Maryam-Yumna <maryamyumna@gmail.com>
2021-08-15 21:59:27 +0530

    react project created - frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SeriniTea</h1>
+      <Router>
+        <Switch>
+
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
