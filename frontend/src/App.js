5e227f650a4068098f4132bd018e6535faac78eb
upulka <67848552+upulka@users.noreply.github.com>
2021-08-19 15:55:18 +0530

    Navigation Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
+import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from "./components/Home/Home";
+import Navbar from "./components/nav bar/Navbar";
+import Cart from "./components/ShoppingCart/Cart";
+import Header from "./components/header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>SeriniTea</h1>
-      <Router>
-        <Switch>
-
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <Router>
+            <Header/>
+            <Navbar/>
+            <Route path = "/" exact>
+                <Home/>
+            </Route>
+            <Route path = "/cart" >
+                <Cart/>
+            </Route>
+        </Router>
+    )
 }
 
 export default App;
---
+++ frontend/src/components/ShoppingCart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Cart(){
+    return <div className= 'container'>
+            <h1>Cart</h1>
+            <p>
+               HI
+            </p>
+      </div>
+}
+
+export default Cart;
