47c1111a126cc49df547f1fbbf9dc7c0bc82ac58
Isini-Dananjana <isinidan97@gmail.com>
2021-08-19 20:49:04 +0530

    Implement admin header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import adminHome from './components/adminHome'
+
 
 function App() {
   return (
+    
     <div className="App">
-      <h1>SeriniTea</h1>
+     
       <Router>
         <Switch>
 
+        <Route path = "/adminHome" component = {adminHome}></Route>
         </Switch>
       </Router>
     </div>
