8abd95d9805deb12926bfcb235f4d5b5b6bebed3
Maryam-Yumna <maryamyumna@gmail.com>
2021-08-26 02:44:26 +0530

    removed admin header in adminhome component

--- frontend/src/components/adminHome.js
+++ frontend/src/components/adminHome.js
@@ -10,7 +10,7 @@ class adminHome extends Component {
   render() {
     return (
       <div>
-        <AdminHeader />
+        {/* <AdminHeader /> */}
         <div class="row">
           <div class="col-sm-2">
             <div
