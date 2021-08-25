465d7621dfc866f51d977c671e2507f240bee8e1
Maryam-Yumna <maryamyumna@gmail.com>
2021-08-26 02:37:01 +0530

    modified header for logged users

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 import '../../App.css';
 import TeaBack from 'url:../../../public/images/TeaBack.jpg';
 
-function Header(){
+function Header(props){
     return<div>
         <section>
             <div className = 'text-white ' class ='header'>
@@ -17,9 +17,19 @@ function Header(){
 
                         <div class = "rightMenu">
                             <ul>
-                                <li class="nav-item navbar-brand "><Link to = "/login">Login</Link></li>
-                                <li class="nav-item navbar-brand "><Link to = "/register">Register</Link></li>
-                                <li class="nav-item navbar-brand "><Link to = "/cart">My Cart</Link></li>
+                                {/* <li class="nav-item navbar-brand "><Link to = "/login">Login</Link></li>
+                                <li class="nav-item navbar-brand "><Link to = "/register">Register</Link></li> */}
+                                {props.logged ==="true"?(<div>
+                                    <li class="nav-item navbar-brand "><Link to = "/myAccount">My Account</Link></li>
+                                    <li class="nav-item navbar-brand " onClick={props.logout}><Link to = "/">Logout</Link></li>
+                                    <li class="nav-item navbar-brand "><Link to = "/cart">My Cart</Link></li>
+                                </div>):
+                                <div>
+                                    <li class="nav-item navbar-brand "><Link to = "/login">Login</Link></li>
+                                    <li class="nav-item navbar-brand "><Link to = "/register">Register</Link></li>
+                                    <li class="nav-item navbar-brand "><Link to = "/cart">My Cart</Link></li>
+                                </div>}
+                                
                             </ul>
                         </div>
                     </div>
