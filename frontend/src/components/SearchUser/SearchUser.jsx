53fa41d930f96efc5f9b7d0ec891a716a27da97f
Maryam-Yumna <maryamyumna@gmail.com>
2021-09-07 00:32:19 +0530

    added a method convert date to readable format in searchUser.jsx

--- frontend/src/components/SearchUser/SearchUser.jsx
+++ frontend/src/components/SearchUser/SearchUser.jsx
@@ -9,6 +9,7 @@ class SearchUser extends Component {
            Users:[],
         }
         this.getAllUsers = this.getAllUsers.bind(this);
+        this.convertDate = this.convertDate.bind(this);
     }
     componentDidMount(){
         this.getAllUsers();
@@ -27,14 +28,32 @@ class SearchUser extends Component {
             console.error(err);
         });
     }
+    convertDate(date){
+        
+        var date1 = new Date(date);
+        console.log(date1)
+        var year = date1.getFullYear();
+        var month =(date1.getMonth() + 1);
+        if(month<10){
+            month = "0"+(month);
+        }
+        
+        var day = date1.getDate();
+        if(day<10){
+            day = "0"+(day);
+        }
+        console.log("date.getDate()", date1.getDate());
+        var convertedDate = year + '-' + month + '-' + day;
+        console.log("payment date", convertedDate);
+        return(convertedDate);
+    }
     render() {
         return (
             <div>
                 <div>
                 <div className="container w-100 p-3 text-center" id="users">
-                        <h2>Search User</h2>
-                        {/* <button id='btnAddNewExpense' type="button" className="btn btn-success">Add New Expense</button> */}
-                        <div className= "container w-25 p-3">
+                        <h2 className="fw-bold mb-0">Search User</h2>
+                        <div className= "container w-25 p-3 mt-2 mb-2">
                             <form className="d-flex">
                                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                                 <button className="btn btn-outline-success" type="submit">Search</button>
@@ -61,7 +80,7 @@ class SearchUser extends Component {
                                                 <td id ='trEmail'>{user.email}</td>
                                                 <td id ='trPhone'>{user.phone}</td>
                                                 <td id ='trAddress'>{user.address}</td>
-                                                <td id ='trRegistredDate'>{user.registeredDate}</td>
+                                                <td id ='trRegistredDate'>{this.convertDate(user.registeredDate)}</td>
                                             </tr>
                                         );
                                     })}
