154c467134651cdf2c9e2a0ba248f7ceebd482f1
upulka <67848552+upulka@users.noreply.github.com>
2021-09-26 18:50:07 +0530

    Icons added to the header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,6 +3,10 @@ import {Link} from 'react-router-dom';
 import '../../App.css';
 import TeaBack from 'url:../../../public/images/TeaBack.jpg';
 import cartIcon from 'url:../../../public/Icons/ic_cart.svg';
+import loginIcon from 'url:../../../public/Icons/ic_login.svg';
+import logoutIcon from 'url:../../../public/Icons/ic_logout.svg';
+import signupIcon from 'url:../../../public/Icons/ic_signup.svg';
+import userAccount from 'url:../../../public/Icons/ic_user_account.svg';
 
 function Header(props){
     return<div>
@@ -22,8 +26,16 @@ function Header(props){
                                 {/* <li class="nav-item navbar-brand "><Link to = "/login">Login</Link></li>
                                 <li class="nav-item navbar-brand "><Link to = "/register">Register</Link></li> */}
                                 {props.logged ==="true"?(<div>{/*When the user logged in*/}
-                                    <li class="nav-item navbar-brand "><Link to = "/myAccount">My Account</Link></li>
-                                    <li class="nav-item navbar-brand " onClick={props.logout}><Link to = "/">Logout</Link></li>
+                                    <li class="nav-item navbar-brand ">
+                                        <Link to = "/myAccount">
+                                            <img src = {userAccount} alt="" width = "20"/> Profile
+                                        </Link>
+                                    </li>
+                                    <li class="nav-item navbar-brand " onClick={props.logout}>
+                                        <Link to = "/">
+                                            <img src = {logoutIcon} alt="" width = "20"/> Logout
+                                        </Link>
+                                    </li>
                                     <li class="nav-item navbar-brand ">
                                         <Link to = "/cart">
                                             <img src = {cartIcon} alt="" width = "20"/>  My Cart
@@ -31,8 +43,16 @@ function Header(props){
                                     </li>
                                 </div>):
                                 <div>{/*When the user logged out*/}
-                                    <li class="nav-item navbar-brand "><Link to = "/login">Login</Link></li>
-                                    <li class="nav-item navbar-brand "><Link to = "/register">Register</Link></li>
+                                    <li class="nav-item navbar-brand ">
+                                        <Link to = "/login">
+                                            <img src = {loginIcon} alt="" width = "20"/> Login
+                                        </Link>
+                                    </li>
+                                    <li class="nav-item navbar-brand ">
+                                        <Link to = "/register">
+                                            <img src = {signupIcon} alt="" width = "20"/> Register
+                                        </Link>
+                                    </li>
                                     <li class="nav-item navbar-brand ">
                                         <Link to = "/cart">
                                             <img src = {cartIcon} alt="" width = "20"/>  My Cart
