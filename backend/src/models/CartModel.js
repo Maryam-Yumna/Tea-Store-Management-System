44928c6f1f66f3aeb958a99aafdc39d5ff6eb95b
upulka <67848552+upulka@users.noreply.github.com>
2021-09-29 23:43:09 +0530

    Add new attribute to cart

--- backend/src/models/CartModel.js
+++ backend/src/models/CartModel.js
@@ -2,9 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const cartSchema = new Schema( {
-    _id: {
-            type: String,
-            required: true,
+//    _id: {
+//            type: String,
+//            required: true,
+//    },
+
+    item: {
+        type: Schema.Types.ObjectId,
+        ref: "Product"
     },
     productName: {
         type: String,
@@ -24,12 +29,12 @@ const cartSchema = new Schema( {
     },
     qty:{
         type: Number
-    }
+    },
     //User's token
-    /*user: {
+    user: {
         type: Schema.Types.ObjectId,
         ref: "User"
-    }*/
+    }
 });
 
 const Cart = mongoose.model("Cart" , cartSchema);
