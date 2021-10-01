2f8ba0da15a1404a21e899c9b74a5e05cb405d88
shriasi <42972697+shriasi@users.noreply.github.com>
2021-10-01 17:17:02 +0530

    employee backend added

---
+++ backend/src/models/Employee.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const employeeSchema = new Schema({
+  EmpFirstName: {
+    type: String,
+    required: true,
+  },
+  EmpLastName: {
+    type: String,
+    required: true,
+  },
+  Address: {
+    type: String,
+    required: true,
+  },
+  Gender: {
+    type: String,
+    required: true,
+  },
+  ContactNumber: {
+    type: String,
+    required: true,
+  },
+  Region: {
+    type: String,
+    required: true,
+  },
+  AvailableTime: {
+    type: String,
+    required: true,
+  },
+  JobType: {
+    type: String,
+    required: true,
+  },
+});
+const Employee = mongoose.model("Employee", employeeSchema);
+module.exports = Employee;
