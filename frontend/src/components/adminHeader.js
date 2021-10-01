6667a2ac779886d47e22a5a538a3b81ff83e488c
Maryam Yumna <it19055404@my.sliit.lk>
2021-10-01 19:51:33 +0530

    added logout implementation in admin header

--- frontend/src/components/adminHeader.js
+++ frontend/src/components/adminHeader.js
@@ -22,10 +22,8 @@ class adminHeader extends Component {
               <div class="col-sm-4 offset-md-1 py-4">
                 <h4 class="text-white">Home</h4>
                 <ul class="list-unstyled">
-                  <li>
-                    <a href="#" class="text-white">
-                      Sign Out
-                    </a>
+                  <li data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <Link class="text-white" to = "/">Logout</Link>
                   </li>
                   <li>
                     <a href="#" class="text-white">
@@ -163,6 +161,24 @@ class adminHeader extends Component {
               </ul>
             </li>
             </ul>
+          </div>
+          <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog modal-sm">
+                  <div className="modal-content">
+                  <div className="modal-header" style={{backgroundColor: "#f54e42", color: "white"}}>
+                  <svg className="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:" id="exclamation-triangle-fill" fill="white" viewBox="0 0 16 16">
+                      <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                      </svg>
+                      <h5 className="modal-title" id="exampleModalLabel">Do you want to logout?</h5>
+                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div className="modal-footer">
+                      <button type="button" className="btn" style={{backgroundColor: "#f54e42", color: "white"}} onClick={this.props.logout} >Yes, Logout</button>
+                      <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      
+                  </div>
+                  </div>
+              </div>
           </div>
         </nav>
 
